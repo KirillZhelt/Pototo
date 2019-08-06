16ad68598d501e792bff385a2e69177a846087d0
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-08-06 13:07:35 +0300

    Create special methods to manage timer

--- app/src/main/java/dev/kirillzhelt/pototo/MainActivity.kt
+++ app/src/main/java/dev/kirillzhelt/pototo/MainActivity.kt
@@ -2,26 +2,53 @@ package dev.kirillzhelt.pototo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var timerTextView: TextView
+    private lateinit var potatoesImageView: ImageView
+    private lateinit var cancelButton: Button
+
+    private lateinit var timer: PototoTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timerTextView: TextView = findViewById(R.id.timer_textview)
+        timerTextView = findViewById(R.id.timer_textview)
 
-        val timer: PototoTimer = PototoTimer(1 * 60 * 1000,
-            { timerTextView.text = "now you are free (really not)" },
-            { p0 -> timerTextView.text = "${p0 / 60000}:${(p0 % 60000) / 1000}" })
+        // TODO: get default remain time from resources and use it here
+        timer = PototoTimer(1 * 60 * 1000,
+            ::timerFinish,
+            ::timerTick)
 
-        val potatoesImageView: ImageView = findViewById(R.id.potatoes_imageview)
-        potatoesImageView.setOnClickListener { timer.start() }
+        potatoesImageView = findViewById(R.id.potatoes_imageview)
+        potatoesImageView.setOnClickListener(::timerStart)
 
-        val cancelButton: Button = findViewById(R.id.cancel_button)
-        cancelButton.setOnClickListener { timer.cancel() }
+        cancelButton = findViewById(R.id.cancel_button)
+        cancelButton.setOnClickListener(::timerCancel)
+    }
+
+    private fun timerStart(v: View) {
+        timer.start()
+        cancelButton.visibility = View.VISIBLE
+    }
+
+    private fun timerTick(p0: Long) {
+        timerTextView.text = "${p0 / 60000}:${(p0 % 60000) / 1000}"
+    }
+
+    private fun timerFinish() {
+        timerTextView.text = "now you are free (really not)"
+        cancelButton.visibility = View.INVISIBLE
+    }
+
+    private fun timerCancel(v: View) {
+        timer.cancel()
+        cancelButton.visibility = View.INVISIBLE
     }
 }
