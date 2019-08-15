f6a4da202a4e1b2a6797fad5be475743d3b3bb63
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-08-15 14:50:02 +0300

    Save preferences to SharedPreferences

--- app/src/main/java/dev/kirillzhelt/pototo/MainActivity.kt
+++ app/src/main/java/dev/kirillzhelt/pototo/MainActivity.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 
 class MainActivity: AppCompatActivity() {
 
@@ -21,6 +23,12 @@ class MainActivity: AppCompatActivity() {
 
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
+
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        val switchPref = sharedPref.getBoolean(SettingsActivity.KEY_PREF_EXAMPLE_SWITCH, false)
+        Toast.makeText(this, switchPref.toString(), Toast.LENGTH_LONG).show()
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp()
