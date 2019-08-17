7f3811fa2fe8f906e3960f869476a9037f4516b8
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-08-17 14:51:21 +0300

    Create xml layout resource file for time picker dialog

---
+++ app/src/main/java/dev/kirillzhelt/pototo/TimePickerPreference.kt
@@ -0,0 +1,12 @@
+package dev.kirillzhelt.pototo
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.preference.DialogPreference
+
+class TimePickerPreference(context: Context, attrs: AttributeSet) : DialogPreference(context, attrs) {
+
+
+
+
+}
